ca4035642fc879dfd548eae8fcafd594bdb60b62
徐勇 <yong1.xu@casstime.com>
2021-08-02 14:40:35 +0800

    初始化代码

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="json_verify",
+    version="1.0.0",
+    packages=find_packages(),
+    description='A JSON contract tool that outputs build matches',
+    author="Ze Hua",
+    author_email = '1737985326@qq.com',
+    url='https://github.com/DaoSen-v/JsonDiff',
+    license='MIT',
+    zip_safe = False,
+    install_requires = [
+    ],
+    entry_points={
+    }
+)
